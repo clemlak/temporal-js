9351751ccebc8c3225e0138ab941bb156c829a50
Clement Lakhal <clementlakhal@gmail.com>
2019-01-27 21:28:46 +0100

    Updated example

--- examples/basic.js
+++ examples/basic.js
@@ -2,19 +2,30 @@ const Temporal = require('..');
 
 const temporal = new Temporal();
 
+temporal.register('jasper12222', 'jasper12222@test.com', 'hello')
+  .then((res) => {
+    console.log('Registred!', res);
 
-temporal.register('bob2019', 'bob2019@bob.co', 'password')
+    return temporal.login('jasper12222', 'hello');
+  })
+  .then((res) => {
+    console.log('Logged in', res);
+  })
+  .catch((err) => {
+    console.log('Error:', err);
+  });
+
+/*
+temporal.login('meh2', 'hello')
+  .then((res) => {
+    console.log(res);
+
+    return temporal.generateIpfsKey('rsa', '2018', 'test');
+  })
   .then((res) => {
     console.log(res);
   })
   .catch((err) => {
-    console.log(err);
+    console.log('Error', err);
   });
-
-temporal.login('bob2019', 'password')
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+*/
